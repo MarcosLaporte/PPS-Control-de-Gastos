d1e4718d4a7cf843f61989c43bdea79cd8a7dbc5
LapMarcos <marcoslaporte2015@gmail.com>
2024-05-29 01:22:08 -0300

    Added firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,19 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { firebase } from 'src/fireApiKey';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
-  bootstrap: [AppComponent],
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    provideFirebaseApp(() => initializeApp(firebase)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore())
+  ]
 })
-export class AppModule {}
+export class AppModule { }
