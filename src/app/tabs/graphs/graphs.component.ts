cac354795dcee718a1ebbc9a20a9d82577524ca2
LapMarcos <marcoslaporte2015@gmail.com>
2024-05-31 02:11:33 -0300

    Pie chart for expenses

--- src/app/tabs/graphs/graphs.component.ts
+++ src/app/tabs/graphs/graphs.component.ts
@@ -1,13 +1,71 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { Chart } from 'chart.js/auto';
+import { Expense } from 'src/app/interfaces';
+import { BudgetService } from 'src/app/services/budget.service';
 
+declare interface CategoryTotal {
+  category: string;
+  totalValue: number;
+}
 @Component({
   selector: 'app-graphs',
   templateUrl: './graphs.component.html',
   styleUrls: ['./graphs.component.scss'],
 })
-export class GraphsComponent {
+export class GraphsComponent implements AfterViewInit {
+  expensesChart?: Chart<"pie", number[], string>;
+  constructor(private budgetServ: BudgetService) {
+  }
 
-  constructor() { }
+  ngAfterViewInit(): void {
+    this.budgetServ.expensesObs.subscribe(() => {
+      if (this.expensesChart) this.expensesChart.destroy();
 
+      this.loadChart();
+    })
+  }
 
+  loadChart() {
+    const canvaEl = document.getElementById('expenses')! as HTMLCanvasElement;
+
+    const data = this.calculateCategoryTotals(this.budgetServ.Expenses);
+    this.expensesChart = new Chart(canvaEl, {
+      type: 'pie',
+      data: {
+        labels: data.map(row => row.category),
+        datasets: [{
+          label: 'Total $',
+          data: data.map(row => row.totalValue),
+          backgroundColor: this.getRandomColors(data.length)
+        }]
+      }
+    });
+  }
+
+  private calculateCategoryTotals(expenses: Expense[]): CategoryTotal[] {
+    const categoryMap = new Map<string, number>();
+
+    for (const exp of expenses) {
+      const currentTotal = categoryMap.get(exp.category) || 0;
+      categoryMap.set(exp.category, currentTotal + exp.value);
+    }
+
+    const result: CategoryTotal[] = [];
+    for (const [category, totalValue] of categoryMap.entries()) {
+      result.push({ category, totalValue });
+    }
+
+    return result;
+  }
+
+  private getRandomColors(amount: number = 1) {
+    const colors = [];
+
+    for (let i = 0; i < amount; i++) {
+      const randomColor = `hsl(${360 * Math.random()},${25 + 70 * Math.random()}%,${85 + 10 * Math.random()}%`;
+      colors.push(randomColor);
+    }
+
+    return colors;
+  }
 }
